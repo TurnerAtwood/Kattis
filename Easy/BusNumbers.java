840167b8789934ec8a78a3aaa6fdd4904b2bba4a
Turner Atwood <bta0008@auburn.edu>
2019-02-19 16:39:25 -0600

    Pattern Matching and searching sorted lists

--- Easy/BusNumbers.java
+++ Easy/BusNumbers.java
@@ -1,64 +1,70 @@
-/*  Turner Atwood
- *  10/05/16
- *  Bus Numbers [2.5] (https://open.kattis.com/problems/busnumbers)
- *  Very old, not clean at all
+/*	Turner Atwood
+ *	2/19/19
+ *	Bus Numbers [2.6]: (https://open.kattis.com/problems/busnumbers2)
+ *	Try all numbers below M by generating the cubes
+ *		up to 400000 and searching from both ends
  */
 
 import java.util.*;
 
-public class BusNumbers {
-  public static void main (String args[]) {
-    Scanner in = new Scanner(System.in);
-    int num = Integer.parseInt(in.nextLine());
-    String[] buss = in.nextLine().split(" ");
-    int[] busses = new int[num];
-    for (int i = 0; i < num; i++) {
-      busses[i] = Integer.parseInt(buss[i]);
-    }
-    Arrays.sort(busses);
-    int count = 1;
-    String print = "";
-    int look = busses[0];
-    int start = look;
-    int last = look;
-    if (num == 1) {
-      print += look;
-    }
-    for (int i = 1; i < num; i++) {
-      look = busses[i];
-      if ( look == start + count) {
-        count++;
-      }
-      else {
-        if (start != last) {
-          if (count > 2) {
-            print += start + "-" + last + " ";
-          }
-          else {
-            print += start + " " + last + " ";
-          }
-        }
-        else {
-          print += start + " ";
-        }
-        count = 1;
-        start = look;
-      }
-      if (i == num - 1) {
-        if (start != look) {
-          if (count > 2) {
-            print += start + "-" + look;
-          }
-          else {
-            print += start + " " + look + " ";
-          }
-        }
-        else {
-          print += start;
-        }
-      }
-      last = look;
-    }
-    System.out.println(print);
-  }
+class BusNumbers {
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int M = in.nextInt();
+		ArrayList<Integer> cubes = generateCubes();
+
+		// Find the highest bus number under N
+		int found = 0;
+		for (int i = M; i >= 0; i--) {
+			if (isBusNumber(i, cubes)) {
+				found = i;
+				break;
+			}
+		}
+
+		if (found != 0) {
+			System.out.println(found);
+		}
+		else {
+			System.out.println("none");	
+		}
+	}
+
+	// List of all cubes under 400,000
+	static ArrayList<Integer> generateCubes() {
+		ArrayList<Integer> cubes = new ArrayList<Integer>();
+		int cube = 1;
+		int i = 2;
+		while (cube < 400000) {
+			cubes.add(cube);
+			cube = i*i*i;
+			i++;
+		}
+		return cubes;
+	}
+
+	// Determine if a number is a bus number
+	static boolean isBusNumber(int a, ArrayList<Integer> cubes) {
+		int left = 0;
+		int right = cubes.size()-1;
+		int count = 0;
+		int potential;
+		while (left <= right) {
+			potential = cubes.get(left) + cubes.get(right);
+			if (potential < a) {
+				left++;
+			}
+			else if (potential > a) {
+				right--;
+			}
+			else {
+				count++;
+				left++;
+				if (count == 2) {
+					return true;
+				}
+			} 
+		}
+		return false;
+	}
 }
